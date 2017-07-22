837755315bda8afb3c7c3a2ccabb05ddfe141fa4
shanmugapriyaeswar <shanmugapriyaeswar1997@gmail.com>
2017-07-22 10:03:06 +0530

    Add files via upload

---
+++ Withoutconcat.java
@@ -0,0 +1,18 @@
+package code;
+import java.util.*;
+public class Withoutconcat {
+
+	public static void main(String[] args)
+	{
+		String str1;
+		String str2;
+		Scanner s=new Scanner(System.in);
+		str1=s.nextLine();
+		str2=s.nextLine();
+		String str=str1+str2;
+		System.out.println(str);
+		
+
+	}
+
+}
